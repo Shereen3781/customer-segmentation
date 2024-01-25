cb3cb100a90c8db5e7115aad6fcd9f992e71c438
Shereen3781 <110721883+Shereen3781@users.noreply.github.com>
2024-01-26 01:13:48 +0200

    Add files via upload

---
+++ streamlit_App.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+st.cache_data
+
+df= pd.read_csv('final_customer_data_with_recommendations.csv')
+
+# Add a multiselect widget to select rows based on the index
+customer= sorted(df.User_Id.unique())
+selected_indices = st.multiselect('Select customer:', options=customer)
+
+# Subset the dataframe with the selected indices
+selected_rows = df.loc[df['User_Id'].isin(selected_indices)]
+
+# Display the selected data
+st.write('Selected customer:')
+st.column_config.Column(width="large")
+st.dataframe(selected_rows.drop('User_Id', axis =1), hide_index=True)
